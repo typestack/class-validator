254dcde12782b8043cee25eefd0c40c5b88cd53c
Umed Khudoiberdiev <zarrhost@gmail.com>
2016-07-06 14:23:08 +0500

    added UseContainerOptions and container fallback ability

--- src/index.ts
+++ src/index.ts
@@ -15,10 +15,27 @@ import {ValidationArguments} from "./validation/ValidationArguments";
 // -------------------------------------------------------------------------
 
 /**
+ * Container options.
+ */
+export interface UseContainerOptions {
+
+    /**
+     * If set to true, then default container will be used in the case if given container haven't returned anything.
+     */
+    fallback?: boolean;
+
+    /**
+     * If set to true, then default container will be used in the case if given container thrown an exception.
+     */
+    fallbackOnErrors?: boolean;
+
+}
+
+/**
  * Container to be used by this library for inversion control. If container was not implicitly set then by default
  * container simply creates a new instance of the given class.
  */
-let container: { get<T>(someClass: { new (...args: any[]): T }|Function): T } = new (class {
+const defaultContainer: { get<T>(someClass: { new (...args: any[]): T }|Function): T } = new (class {
     private instances: any[] = [];
     get<T>(someClass: { new (...args: any[]): T }): T {
         if (!this.instances[someClass as any])
@@ -28,20 +45,36 @@ let container: { get<T>(someClass: { new (...args: any[]): T }|Function): T } = 
     }
 })();
 
+let userContainer: { get<T>(someClass: { new (...args: any[]): T }|Function): T };
+let userContainerOptions: UseContainerOptions;
+
 /**
  * Sets container to be used by this library.
- *
- * @param iocContainer
  */
-export function useContainer(iocContainer: { get(someClass: any): any }) {
-    container = iocContainer;
+export function useContainer(iocContainer: { get(someClass: any): any }, options?: UseContainerOptions) {
+    userContainer = iocContainer;
+    userContainerOptions = options;
 }
 
 /**
  * Gets the IOC container used by this library.
  */
 export function getFromContainer<T>(someClass: { new (...args: any[]): T }|Function): T {
-    return container.get<T>(someClass);
+    if (userContainer) {
+        try {
+            const instance = userContainer.get(someClass);
+            if (instance)
+                return instance;
+
+            if (!userContainerOptions || !userContainerOptions.fallback)
+                return instance;
+
+        } catch (error) {
+            if (!userContainerOptions || !userContainerOptions.fallbackOnErrors)
+                throw error;
+        }
+    }
+    return defaultContainer.get<T>(someClass);
 }
 
 // -------------------------------------------------------------------------
