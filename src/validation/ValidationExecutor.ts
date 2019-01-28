b00c1cce031fa0861c43760d3f48df81f275a6ea
andrewwa-kainos <andrewwa@kainos.com>
2019-01-28 13:43:25 +0000

    Use @typed/is-promise isPromise(x) instead of instanceof Promise

--- src/validation/ValidationExecutor.ts
+++ src/validation/ValidationExecutor.ts
@@ -8,6 +8,7 @@ import {ValidationTypes} from "./ValidationTypes";
 import {ConstraintMetadata} from "../metadata/ConstraintMetadata";
 import {ValidationArguments} from "./ValidationArguments";
 import {ValidationUtils} from "./ValidationUtils";
+import { isPromise } from "@typed/is-promise";
 
 /**
  * Executes validation over given object.
@@ -234,7 +235,7 @@ export class ValidationExecutor {
                         constraints: metadata.constraints
                     };
                     const validatedValue = customConstraintMetadata.instance.validate(value, validationArguments);
-                    if (validatedValue instanceof Promise) {
+                    if (isPromise(validatedValue)) {
                         const promise = validatedValue.then(isValid => {
                             if (!isValid) {
                                 const [type, message] = this.createValidationError(object, value, metadata, customConstraintMetadata);
