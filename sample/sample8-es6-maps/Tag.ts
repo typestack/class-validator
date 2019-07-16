4fa4e72cbbbb128b8dd0454965757ec8895db358
Tarik02 <Taras.Fomin@gmail.com>
2019-07-16 18:51:45 +0300

    Add map and set samples

---
+++ sample/sample8-es6-maps/Tag.ts
@@ -0,0 +1,10 @@
+import {Contains, IsInt, Length, IsEmail, IsFQDN, IsDate} from "../../src/decorator/decorators";
+
+export class Tag {
+
+    @Length(10, 20, {
+        message: "Tag value is too short or long"
+    })
+    value: string;
+
+}
