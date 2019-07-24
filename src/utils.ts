e53e82f30d01bdd9b9e58c1b66019a90a9ff670a
Andrew Walker <andrewwa@kainos.com>
2019-07-24 10:42:03 +0100

    Simplified method overloading with default generic types

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,5 @@
 // https://github.com/TylorS/typed-is-promise/blob/abf1514e1b6961adfc75765476b0debb96b2c3ae/src/index.ts
 
-export function isPromise<T>(p: any): p is Promise<T>;
-export function isPromise(p: any): p is Promise<any>;
-
-export function isPromise<T>(p: any): p is Promise<T> {
+export function isPromise<T = any>(p: any): p is Promise<T> {
     return p !== null && typeof p === "object" && typeof p.then === "function";
 }
