415f4eb933ab7aa85d4792a1fe2df9a5fa6f08f9
Bogdan Gusiev <agresso@gmail.com>
2020-04-30 18:17:05 +0300

    Allow readonly array argument for IsIn IsNotIn validators

--- src/decorator/common/IsNotIn.ts
+++ src/decorator/common/IsNotIn.ts
@@ -6,14 +6,14 @@ export const IS_NOT_IN = "isNotIn";
 /**
  * Checks if given value not in a array of allowed values.
  */
-export function isNotIn(value: unknown, possibleValues: unknown[]): boolean {
+export function isNotIn(value: unknown, possibleValues: readonly unknown[]): boolean {
     return !(possibleValues instanceof Array) || !possibleValues.some(possibleValue => possibleValue === value);
 }
 
 /**
  * Checks if given value not in a array of allowed values.
  */
-export function IsNotIn(values: any[], validationOptions?: ValidationOptions): PropertyDecorator {
+export function IsNotIn(values: readonly any[], validationOptions?: ValidationOptions): PropertyDecorator {
     return ValidateBy(
         {
             name: IS_NOT_IN,
