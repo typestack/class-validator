749fb26281a73ef43ce7e297f8b92be8a419c8cb
Attila Olah <NoNameProvided@users.noreply.github.com>
2020-08-02 21:15:47 +0200

    build: update build tooling

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: 'build/esm5/index.js',
+  output: [
+    {
+      name: 'ClassValidator',
+      format: 'umd',
+      file: 'build/bundles/class-validator.umd.js',
+      sourcemap: true,
+    },
+    {
+      name: 'ClassValidator',
+      format: 'umd',
+      file: 'build/bundles/class-validator.umd.min.js',
+      sourcemap: true,
+      plugins: [terser()],
+    },
+  ],
+  plugins: [nodeResolve()],
+};
