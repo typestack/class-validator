5ff067505afd919fe305e736560d119c152f2376
tldrd0117 <tldrd0117@gmail.com>
2020-08-23 18:59:37 +0900

    add arrayincludes

--- src/decorator/decorators.ts
+++ src/decorator/decorators.ts
@@ -135,6 +135,7 @@ export * from './array/ArrayNotEmpty';
 export * from './array/ArrayMinSize';
 export * from './array/ArrayMaxSize';
 export * from './array/ArrayUnique';
+export * from './array/ArrayIncludes';
 
 // -------------------------------------------------------------------------
 // Object checkers
