cc3890b9370527eedb56ef0b2425008a7e5e72f8
tldrd0117 <tldrd0117@gmail.com>
2020-09-07 23:39:02 +0900

    feat: add arrayincludes

---
+++ src/decorator/array/ArrayIncludes.ts
@@ -0,0 +1,35 @@
+import { ValidationOptions } from '../ValidationOptions';
+import { buildMessage, ValidateBy } from '../common/ValidateBy';
+
+export const ARRAY_INCLUDES = 'arrayIncludes';
+
+/**
+ * Checks if all values from the given array of values include some of array's values.
+ * If null or undefined is given then this function returns false.
+ */
+export function arrayIncludes(array: unknown, values: any[]): boolean {
+  if (!(array instanceof Array)) return false;
+
+  return array.every(value => values.includes(value));
+}
+
+/**
+ * Checks if all values from the given array of values include some of array's values.
+ * If null or undefined is given then this function returns false.
+ */
+export function ArrayIncludes(values: any[], validationOptions?: ValidationOptions): PropertyDecorator {
+  return ValidateBy(
+    {
+      name: ARRAY_INCLUDES,
+      constraints: [values],
+      validator: {
+        validate: (value, args): boolean => arrayIncludes(value, args.constraints[0]),
+        defaultMessage: buildMessage(
+          eachPrefix => eachPrefix + '$property must include some of $constraint1 values',
+          validationOptions
+        ),
+      },
+    },
+    validationOptions
+  );
+}
