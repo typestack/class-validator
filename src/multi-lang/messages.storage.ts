8f3931e718c41dbf2b5158907aeae178aa6e2a14
EndyKaufman <admin@site15.ru>
2020-09-11 00:27:54 +0400

    feat: add storages for titles and messages

--- src/multi-lang/messages.storage.ts
+++ src/multi-lang/messages.storage.ts
@@ -1,19 +1,26 @@
 import { getGlobal } from '../utils/get-global.util';
 
+const CLASS_VALIDATOR_MESSAGES = 'CLASS_VALIDATOR_MESSAGES';
+
+export function getClassValidatorMessagesStorage(): { [key: string]: string } {
+  const global: { [CLASS_VALIDATOR_MESSAGES]: { [key: string]: string } } = getGlobal();
+  if (!global[CLASS_VALIDATOR_MESSAGES]) {
+    global[CLASS_VALIDATOR_MESSAGES] = {};
+  }
+  return global[CLASS_VALIDATOR_MESSAGES];
+}
+
 export function setClassValidatorMessages(messages: { [key: string]: string }) {
-  const global = getGlobal();
-  global.CLASS_VALIDATOR_MESSAGES = messages;
+  const storageMessages = getClassValidatorMessagesStorage();
+  Object.keys(storageMessages).forEach(key => delete storageMessages[key]);
+  Object.assign(storageMessages, messages);
 }
 
 export function getClassValidatorMessages(): { [key: string]: string } {
-  const global = getGlobal();
-  if (!global.CLASS_VALIDATOR_MESSAGES) {
-    global.CLASS_VALIDATOR_MESSAGES = {};
-  }
-  return global.CLASS_VALIDATOR_MESSAGES;
+  return getClassValidatorMessagesStorage();
 }
 
 export function getClassValidatorMessage(key: string): string {
-  const messages = getClassValidatorMessages();
+  const messages = getClassValidatorMessagesStorage();
   return messages[key];
 }
--- src/multi-lang/titles.storage.ts
+++ src/multi-lang/titles.storage.ts
@@ -15,26 +15,28 @@ const CLASS_VALIDATOR_PROPERTY_TITLES = 'CLASS_VALIDATOR_PROPERTY_TITLES';
 const CLASS_VALIDATOR_TITLES = 'CLASS_VALIDATOR_TITLES';
 const CLASS_VALIDATOR_ROOT_TITLE = 'CLASS_VALIDATOR_ROOT_TITLE';
 
-export function setClassValidatorPropertyTitle(object: object, propertyName: string | symbol, title: string) {
+// PROPERTY
+export function getClassValidatorPropertyTitlesStorage(): ClassValidatorPropertyTitle[] {
   const global: { [CLASS_VALIDATOR_PROPERTY_TITLES]: ClassValidatorPropertyTitle[] } = getGlobal();
   if (!global[CLASS_VALIDATOR_PROPERTY_TITLES]) {
     global[CLASS_VALIDATOR_PROPERTY_TITLES] = [];
   }
-  let obj: ClassValidatorPropertyTitle | undefined = (global[CLASS_VALIDATOR_PROPERTY_TITLES] || []).find(
-    o => o.target === object.constructor
-  );
+  return global[CLASS_VALIDATOR_PROPERTY_TITLES];
+}
+
+export function setClassValidatorPropertyTitle(object: object, propertyName: string | symbol, title: string) {
+  const storagePropertyTitle = getClassValidatorPropertyTitlesStorage();
+  let obj: ClassValidatorPropertyTitle | undefined = storagePropertyTitle.find(o => o.target === object.constructor);
   if (!obj) {
     obj = { target: object.constructor, titles: new Map() };
-    global[CLASS_VALIDATOR_PROPERTY_TITLES].push(obj);
+    storagePropertyTitle.push(obj);
   }
   obj.titles.set(propertyName, title);
 }
 
 export function getClassValidatorPropertyTitles(object: object): ClassValidatorPropertyTitle['titles'] {
-  const global: { [CLASS_VALIDATOR_PROPERTY_TITLES]: ClassValidatorPropertyTitle[] } = getGlobal();
-  const obj: ClassValidatorPropertyTitle | undefined = (global[CLASS_VALIDATOR_PROPERTY_TITLES] || []).find(
-    o => o.target === object.constructor
-  );
+  const storagePropertyTitle = getClassValidatorPropertyTitlesStorage();
+  const obj: ClassValidatorPropertyTitle | undefined = storagePropertyTitle.find(o => o.target === object.constructor);
   if (!obj) {
     return new Map();
   }
@@ -46,25 +48,29 @@ export function getClassValidatorPropertyTitle(object: object, propertyName: str
   return titles.get(propertyName);
 }
 
-export function setClassValidatorTitle(object: object, propertyName: string | undefined, title: string) {
+// CLASS
+export function getClassValidatorTitlesStorage(): ClassValidatorTitle[] {
   const global: { [CLASS_VALIDATOR_TITLES]: ClassValidatorTitle[] } = getGlobal();
   if (!global[CLASS_VALIDATOR_TITLES]) {
     global[CLASS_VALIDATOR_TITLES] = [];
   }
-  let obj: ClassValidatorTitle | undefined = (global[CLASS_VALIDATOR_TITLES] || []).find(o => o.target === object);
+  return global[CLASS_VALIDATOR_TITLES];
+}
+
+export function setClassValidatorTitle(object: object, propertyName: string | undefined, title: string) {
+  const storageTitle = getClassValidatorTitlesStorage();
+  let obj: ClassValidatorTitle | undefined = storageTitle.find(o => o.target === object);
   if (!obj) {
     obj = { target: object, titles: new Map() };
-    global[CLASS_VALIDATOR_TITLES].push(obj);
+    storageTitle.push(obj);
   }
 
   obj.titles.set(propertyName || CLASS_VALIDATOR_ROOT_TITLE, title);
 }
 
 export function getClassValidatorTitles(object: object): ClassValidatorTitle['titles'] {
-  const global: { [CLASS_VALIDATOR_TITLES]: ClassValidatorTitle[] } = getGlobal();
-  const obj: ClassValidatorTitle | undefined = (global[CLASS_VALIDATOR_TITLES] || []).find(
-    o => o.target === object.constructor
-  );
+  const storageTitle = getClassValidatorTitlesStorage();
+  const obj: ClassValidatorTitle | undefined = storageTitle.find(o => o.target === object.constructor);
   if (!obj) {
     return new Map();
   }
