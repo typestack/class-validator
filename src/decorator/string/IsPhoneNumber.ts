f95a5632f59b969d3ccf6113773f81f658b29ff4
Attila Olah <NoNameProvided@users.noreply.github.com>
2021-01-13 14:06:01 +0100

    style: format code with prettier

--- src/decorator/string/IsPhoneNumber.ts
+++ src/decorator/string/IsPhoneNumber.ts
@@ -7,7 +7,7 @@ export const IS_PHONE_NUMBER = 'isPhoneNumber';
 /**
  * Checks if the string is a valid phone number. To successfully validate any phone number the text must include
  * the intl. calling code, if the calling code wont be provided then the region must be set.
- * 
+ *
  * @param value the potential phone number string to test
  * @param region 2 characters uppercase country code (e.g. DE, US, CH) for country specific validation.
  * If text doesn't start with the international calling code (e.g. +41), then you must set this parameter.
@@ -26,14 +26,11 @@ export function isPhoneNumber(value: string, region?: CountryCode): boolean {
 /**
  * Checks if the string is a valid phone number. To successfully validate any phone number the text must include
  * the intl. calling code, if the calling code wont be provided then the region must be set.
- * 
+ *
  * @param region 2 characters uppercase country code (e.g. DE, US, CH) for country specific validation.
  * If text doesn't start with the international calling code (e.g. +41), then you must set this parameter.
  */
-export function IsPhoneNumber(
-  region?: CountryCode,
-  validationOptions?: ValidationOptions
-): PropertyDecorator {
+export function IsPhoneNumber(region?: CountryCode, validationOptions?: ValidationOptions): PropertyDecorator {
   return ValidateBy(
     {
       name: IS_PHONE_NUMBER,
