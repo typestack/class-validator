2e25aaf0fa33e9f21f533366c4ec51f92ddb7636
Yitzchak Ben-Ezra <yitzchak.ben-ezra@ecoplant.co>
2021-02-23 19:03:25 +0200

    feat: make `IsOptional` conditional

--- test/functional/conditional-validation.spec.ts
+++ test/functional/conditional-validation.spec.ts
@@ -92,4 +92,72 @@ describe('conditional validation', () => {
       expect(errors[0].value).toEqual('bad_value');
     });
   });
+
+  it('should validate a property when condition returns true and value is supplied', function () {
+    class MyClass {
+      @IsOptional(item => item.age === 33)
+      @Equals('test')
+      title: string = 'bad_value';
+      age: number = 33;
+    }
+
+    const model = new MyClass();
+    return validator.validate(model).then(errors => {
+      expect(errors.length).toEqual(1);
+      expect(errors[0].target).toEqual(model);
+      expect(errors[0].property).toEqual('title');
+      expect(errors[0].constraints).toEqual({ equals: 'title must be equal to test' });
+      expect(errors[0].value).toEqual('bad_value');
+    });
+  });
+
+  it('should validate a property when condition returns false and value is supplied', function () {
+    class MyClass {
+      @IsOptional(item => item.age === 34)
+      @Equals('test')
+      title: string = 'bad_value';
+      age: number = 33;
+    }
+
+    const model = new MyClass();
+    return validator.validate(model).then(errors => {
+      expect(errors.length).toEqual(1);
+      expect(errors[0].target).toEqual(model);
+      expect(errors[0].property).toEqual('title');
+      expect(errors[0].constraints).toEqual({ equals: 'title must be equal to test' });
+      expect(errors[0].value).toEqual('bad_value');
+    });
+  });
+
+  it('should validate a property when condition returns true and value is not supplied', function () {
+    class MyClass {
+      @IsOptional(item => item.age === 33)
+      @Equals('test')
+      title: string;
+      age: number = 33;
+    }
+
+    const model = new MyClass();
+    return validator.validate(model).then(errors => {
+      expect(errors.length).toEqual(0);
+    });
+  });
+
+  it('should validate a property when condition returns false and value is not supplied', function () {
+    class MyClass {
+      @IsOptional(item => item.age === 34)
+      @Equals('test')
+      title: string;
+      age: number = 33;
+    }
+
+    const model = new MyClass();
+    return validator.validate(model).then(errors => {
+      expect(errors.length).toEqual(1);
+      expect(errors[0].target).toEqual(model);
+      expect(errors[0].property).toEqual('title');
+      expect(errors[0].constraints).toEqual({ equals: 'title must be equal to test' });
+      expect(errors[0].value).toEqual(undefined);
+    });
+  });
 });
