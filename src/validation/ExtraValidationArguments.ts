b52847740a560652d213599600844bd2619c059b
Artem Troitskiy <axtavt@gmail.com>
2021-03-11 22:41:20 +0300

    fix: typo

--- src/validation/ExtraValidationArguments.ts
+++ src/validation/ExtraValidationArguments.ts
@@ -1,7 +1,7 @@
 /**
  * Extra validation arguments that can be passed from caller of validate() to message builders.
  *
- * This type can be augumented using type merging to pass arguments in typesafe way, e.g.
+ * This type can be augmented using type merging to pass arguments in typesafe way, e.g.
  * ```typescript
  * declare module 'class-validator' {
  *   interface ExtraValidationArguments {
@@ -12,5 +12,5 @@
  */
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface ExtraValidationArguments {
-  // for augumentation with type merging
+  // for augmentation with type merging
 }
