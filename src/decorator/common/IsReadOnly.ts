b26697aedf37011731748f292191d48624e965ae
Grigory Kholstinnikov <holstinnikov@sharkdg.com>
2021-09-04 22:16:24 +0300

    add `IsReadOnly` decorator

---
+++ src/decorator/common/IsReadOnly.ts
@@ -0,0 +1,27 @@
+import { ValidationOptions } from '../ValidationOptions';
+import { buildMessage, ValidateBy } from './ValidateBy';
+
+export const IS_READONLY = 'isReadOnly';
+
+/**
+ * Checks if given value is undefined.
+ */
+export function isReadOnly(value: unknown): boolean {
+  return value === undefined;
+}
+
+/**
+ * Checks if given value is undefined.
+ */
+export function IsReadOnly(validationOptions?: ValidationOptions): PropertyDecorator {
+  return ValidateBy(
+    {
+      name: IS_READONLY,
+      validator: {
+        validate: (value, args): boolean => isReadOnly(value),
+        defaultMessage: buildMessage(eachPrefix => eachPrefix + '$property must be undefined', validationOptions),
+      },
+    },
+    validationOptions
+  );
+}
