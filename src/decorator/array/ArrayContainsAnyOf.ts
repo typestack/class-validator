1b79f5b8bc0968bb294877d0990af0a2a3a755d8
SET001 <settydark@gmail.com>
2021-10-01 20:40:41 +0300

    add ArrayContainsAnyOf decorator

---
+++ src/decorator/array/ArrayContainsAnyOf.ts
@@ -0,0 +1,35 @@
+import { ValidationOptions } from '../ValidationOptions';
+import { buildMessage, ValidateBy } from '../common/ValidateBy';
+
+export const ARRAY_CONTAINS_ANY_OF = 'arrayContainsAnyOf';
+
+/**
+ * Checks if array contains all values from the given array of values.
+ * If null or undefined is given then this function returns false.
+ */
+export function arrayContainsAnyOf(array: unknown, values: any[]): boolean {
+  if (!(array instanceof Array)) return false;
+
+  return array.every(value => values.indexOf(value) !== -1);
+}
+
+/**
+ * Checks if array contains all values from the given array of values.
+ * If null or undefined is given then this function returns false.
+ */
+export function ArrayContainsAnyOf(values: any[], validationOptions?: ValidationOptions): PropertyDecorator {
+  return ValidateBy(
+    {
+      name: 'arrayContainsAny',
+      constraints: [values],
+      validator: {
+        validate: (value, args): boolean => arrayContainsAnyOf(value, args.constraints[0]),
+        defaultMessage: buildMessage(
+          eachPrefix => eachPrefix + '$property values must be on of $constraint1',
+          validationOptions
+        ),
+      },
+    },
+    validationOptions
+  );
+}
