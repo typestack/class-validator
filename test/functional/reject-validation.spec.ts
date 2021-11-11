e0605d65bd1521532c2105515da670666288d516
mikegu <7657388+mikeguta@users.noreply.github.com>
2021-11-11 21:18:29 +0000

    async/await avoids unhandled rejections

--- test/functional/reject-validation.spec.ts
+++ test/functional/reject-validation.spec.ts
@@ -18,22 +18,25 @@ describe('validateOrReject()', () => {
     model = new MyClass();
   });
 
-  it('should resolve promise when no error', () => {
+  it('should resolve promise when no error', async () => {
     expect.assertions(1);
     model.someProperty = 'hello world';
-    return validator.validateOrReject(model).then(args => {
-      expect(args).toBeUndefined();
-    });
+    const args = await validator.validateOrReject(model);
+    expect(args).toBeUndefined();
   });
 
-  it('should reject promise on error', () => {
+  it('should reject promise on error', async () => {
     expect.assertions(2);
     model.someProperty = 'hell no world';
-    return validator.validateOrReject(model).catch((errors: ValidationError[]) => {
-      expect(errors.length).toEqual(1);
-      expect(errors[0].constraints).toEqual({
-        contains: 'hell no world is not valid. Your string must contain a hello word',
-      });
+    let errors: ValidationError[];
+    try {
+      await validator.validateOrReject(model);
+    } catch (caught) {
+      errors = caught;
+    }
+    expect(errors.length).toEqual(1);
+    expect(errors[0].constraints).toEqual({
+      contains: 'hell no world is not valid. Your string must contain a hello word',
     });
   });
 });
