797f707db233574d6bc5ba3820b91b37398b969a
Imran Sasudin <imransasudin@gmail.com>
2022-01-29 18:12:05 +0800

    fix: add ignore option for isAlpha & isAlphanumeric

--- src/decorator/string/IsAlpha.ts
+++ src/decorator/string/IsAlpha.ts
@@ -9,21 +9,21 @@ export const IS_ALPHA = 'isAlpha';
  * Checks if the string contains only letters (a-zA-Z).
  * If given value is not a string, then it returns false.
  */
-export function isAlpha(value: unknown, locale?: ValidatorJS.AlphaLocale): boolean {
-  return typeof value === 'string' && isAlphaValidator(value, locale);
+export function isAlpha(value: unknown, locale?: ValidatorJS.AlphaLocale, options?: ValidatorJS.IsAlphaOptions): boolean {
+  return typeof value === 'string' && isAlphaValidator(value, locale, options);
 }
 
 /**
  * Checks if the string contains only letters (a-zA-Z).
  * If given value is not a string, then it returns false.
  */
-export function IsAlpha(locale?: string, validationOptions?: ValidationOptions): PropertyDecorator {
+export function IsAlpha(locale?: string, validationOptions?: ValidationOptions, options?: ValidatorJS.IsAlphaOptions): PropertyDecorator {
   return ValidateBy(
     {
       name: IS_ALPHA,
-      constraints: [locale],
+      constraints: [locale, options],
       validator: {
-        validate: (value, args): boolean => isAlpha(value, args.constraints[0]),
+        validate: (value, args): boolean => isAlpha(value, args.constraints[0], args.constraints[1]),
         defaultMessage: buildMessage(
           eachPrefix => eachPrefix + '$property must contain only letters (a-zA-Z)',
           validationOptions
