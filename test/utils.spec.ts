66e5ab357ea29b3fde4386a7d7dfb7942c6cb933
Christian Forgács <christian@wunderbit.de>
2023-04-27 18:30:16 +0200

    feat: add util to check variable is object literal

--- test/utils.spec.ts
+++ test/utils.spec.ts
@@ -1,4 +1,4 @@
-import { convertToArray } from '../src/utils';
+import { convertToArray, isObjectLiteral } from '../src/utils';
 
 describe('convertToArray', () => {
   it('convert Set into array', () => {
@@ -29,5 +29,45 @@ describe('convertToArray', () => {
     expect(arr.length).toEqual(2);
     expect(arr).toContain('hello');
     expect(arr).toContain('world');
+  });
+});
+
+describe('isObjectLiteral', () => {
+  it('check is object is object literal', () => {
+    expect(
+      isObjectLiteral({
+        foo: 'bar',
+        bar: 'foo',
+      })
+    ).toEqual(true);
+  });
+
+  it('check is Object is object literal', () => {
+    expect(
+      isObjectLiteral(
+        new Object({
+          foo: 'bar',
+          bar: 'foo',
+        })
+      )
+    ).toEqual(true);
+  });
+
+  it("check is array isn't object literal", () => {
+    expect(isObjectLiteral(['foo', 'bar'])).toEqual(false);
+  });
+
+  it("check is Date isn't object literal", () => {
+    expect(isObjectLiteral(new Date())).toEqual(false);
+  });
+
+  it("check is custom class isn't object literal", () => {
+    class Foo {
+      bar: string;
+    }
+    const fooInstance = new Foo();
+    fooInstance.bar = 'baz';
+
+    expect(isObjectLiteral(fooInstance)).toEqual(false);
   });
 });
