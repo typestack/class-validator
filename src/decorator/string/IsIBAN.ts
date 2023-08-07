e3ade5e0b2e8738f1fec6d62fc74f6fbba1cf56c
edilson <edilson.dasilva@thoughtworks.com>
2023-08-07 17:53:07 -0300

    Add IsIBANOptions to IsIBAN validator

--- src/decorator/string/IsIBAN.ts
+++ src/decorator/string/IsIBAN.ts
@@ -1,6 +1,7 @@
 import { ValidationOptions } from '../ValidationOptions';
 import { buildMessage, ValidateBy } from '../common/ValidateBy';
 import isIBANValidator from 'validator/lib/isIBAN';
+import { IsIBANOptions } from 'validator/lib/isIBAN';
 
 export const IS_IBAN = 'isIBAN';
 
@@ -8,20 +9,21 @@ export const IS_IBAN = 'isIBAN';
  * Check if a string is a IBAN (International Bank Account Number).
  * If given value is not a string, then it returns false.
  */
-export function isIBAN(value: unknown): boolean {
-  return typeof value === 'string' && isIBANValidator(value);
+export function isIBAN(value: unknown, options?: IsIBANOptions): boolean {
+  return typeof value === 'string' && isIBANValidator(value, options);
 }
 
 /**
  * Check if a string is a IBAN (International Bank Account Number).
  * If given value is not a string, then it returns false.
  */
-export function IsIBAN(validationOptions?: ValidationOptions): PropertyDecorator {
+export function IsIBAN(options?: IsIBANOptions, validationOptions?: ValidationOptions): PropertyDecorator {
   return ValidateBy(
     {
       name: IS_IBAN,
+      constraints: [options],
       validator: {
-        validate: (value, args): boolean => isIBAN(value),
+        validate: (value, args): boolean => isIBAN(value, args?.constraints[0]),
         defaultMessage: buildMessage(eachPrefix => eachPrefix + '$property must be an IBAN', validationOptions),
       },
     },
