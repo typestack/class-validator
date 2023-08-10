d4dee05a8c964f6f927bffcdf5b33bfd41905cd8
Chris Duff <chris@praxhub.com>
2023-08-10 22:30:20 +1000

    test: improve validator-options.spec.ts based on PR feedback.

--- test/functional/validator-options.spec.ts
+++ test/functional/validator-options.spec.ts
@@ -8,23 +8,20 @@ describe('validator options', () => {
     class MyClass {
       @IsNotEmpty()
       title: string = '';
-      isActive: boolean;
     }
 
     const model = new MyClass();
     model.title = '';
-    return validator
-      .validate(model, { skipMissingProperties: true, validationError: { target: false } })
-      .then(errors => {
-        expect(errors.length).toEqual(1);
-        expect(errors[0].target).toBeUndefined();
-        expect(errors[0].property).toEqual('title');
-        expect(errors[0].constraints).toEqual({ isNotEmpty: 'title should not be empty' });
-        expect(errors[0].value).toEqual('');
-      });
+    return validator.validate(model, { validationError: { target: false } }).then(errors => {
+      expect(errors.length).toEqual(1);
+      expect(errors[0].target).toBeUndefined();
+      expect(errors[0].property).toEqual('title');
+      expect(errors[0].constraints).toEqual({ isNotEmpty: 'title should not be empty' });
+      expect(errors[0].value).toEqual('');
+    });
   });
 
-  it('should returns error on unknown objects if forbidUnknownValues is true', function () {
+  it('should return error on unknown objects if forbidUnknownValues is true', () => {
     const anonymousObject = { badKey: 'This should not pass.' };
 
     return validator.validate(anonymousObject, { forbidUnknownValues: true }).then(errors => {
@@ -37,7 +34,7 @@ describe('validator options', () => {
     });
   });
 
-  it('should return no error on unknown objects if forbidUnknownValues is false', function () {
+  it('should return no error on unknown objects if forbidUnknownValues is false', () => {
     const anonymousObject = { badKey: 'This should not pass.' };
 
     return validator.validate(anonymousObject, { forbidUnknownValues: false }).then(errors => {
@@ -45,8 +42,29 @@ describe('validator options', () => {
     });
   });
 
-  it(`should forbidUnknownValues by default`, function () {
-    expect(validator.validateSync({}).length).toEqual(1);
-    expect(validator.validateSync({}, {}).length).toEqual(1);
+  it(`should return error on unknown objects if no options object argument is passed`, () => {
+    const anonymousObject = { badKey: 'This should not pass.' };
+
+    return validator.validate(anonymousObject, undefined).then(errors => {
+      expect(errors.length).toEqual(1);
+      expect(errors[0].target).toEqual(anonymousObject);
+      expect(errors[0].property).toEqual(undefined);
+      expect(errors[0].value).toEqual(undefined);
+      expect(errors[0].children).toBeInstanceOf(Array);
+      expect(errors[0].constraints).toEqual({ unknownValue: 'an unknown value was passed to the validate function' });
+    });
+  });
+
+  it(`should return error on unknown objects if options object argument passed with forbidUnknownValues undefined`, () => {
+    const anonymousObject = { badKey: 'This should not pass.' };
+
+    return validator.validate(anonymousObject, { forbidUnknownValues: undefined }).then(errors => {
+      expect(errors.length).toEqual(1);
+      expect(errors[0].target).toEqual(anonymousObject);
+      expect(errors[0].property).toEqual(undefined);
+      expect(errors[0].value).toEqual(undefined);
+      expect(errors[0].children).toBeInstanceOf(Array);
+      expect(errors[0].constraints).toEqual({ unknownValue: 'an unknown value was passed to the validate function' });
+    });
   });
 });
