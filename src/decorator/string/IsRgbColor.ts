a032c59fc844b425c412767ba64d416c1a359ede
Wei Kang <weikangchia@gmail.com>
2025-10-31 22:12:20 +0800

    update

--- src/decorator/string/IsRgbColor.ts
+++ src/decorator/string/IsRgbColor.ts
@@ -10,9 +10,12 @@ export const IS_RGB_COLOR = 'isRgbColor';
  * If given value is not a string, then it returns false.
  */
 export function isRgbColor(value: unknown, includePercentValues?: boolean): boolean {
-  return typeof value === 'string' && isRgbColorValidator(value, {
-    includePercentValues
-  });
+  return (
+    typeof value === 'string' &&
+    isRgbColorValidator(value, {
+      includePercentValues,
+    })
+  );
 }
 
 /**
