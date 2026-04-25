eb3ed9b71eb86af94f5f599b5fe19a0f87b6ddb6
cherites0610 <115400896+cherites0610@users.noreply.github.com>
2026-04-25 19:22:23 +0800

    fix: isISO31661Alpha2 currently allowing lowercase characters (#2629)

--- src/decorator/string/IsISO31661Alpha2.ts
+++ src/decorator/string/IsISO31661Alpha2.ts
@@ -8,7 +8,7 @@ export const IS_ISO31661_ALPHA_2 = 'isISO31661Alpha2';
  * Check if the string is a valid [ISO 3166-1 alpha-2](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2) officially assigned country code.
  */
 export function isISO31661Alpha2(value: unknown): boolean {
-  return typeof value === 'string' && isISO31661Alpha2Validator(value);
+  return typeof value === 'string' && value === value.toUpperCase() && isISO31661Alpha2Validator(value);
 }
 
 /**
